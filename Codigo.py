995f3812c8b529910dacefefcb594f776526d136
Kevenny <kevennykeke@gmail.com>
2020-07-10 09:47:03 -0300

    [Implemented] Word_List and auto select

---
+++ Codigo.py
@@ -0,0 +1,5 @@
+import random
+import Funcoes
+
+_palavra_secreta = Funcoes.Start() # armazena a palavra secreta
+_Palavra_secreta_pontilhada = "_" * len(_palavra_secreta) # armazena a palavra secreta só que em vez de letras, em _
