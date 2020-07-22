755d5b7a06d0a4144d5c19f2f8d7f1fbca5dc8cb
Kevenny <kevennykeke@gmail.com>
2020-07-21 21:10:58 -0300

    [Implemented] Connecting functions

--- Codigo.py
+++ Codigo.py
@@ -1,7 +1,45 @@
 import random
 import Funcoes
+import time
 
 
-_palavra_secreta = Funcoes.Start() # armazena a palavra secreta
-_Palavra_secreta_pontilhada = "_" * len(_palavra_secreta) # armazena a palavra secreta só que em vez de letras, em _
-Funcoes.Lista_Palavras_Usadas.append(_palavra_secreta) #Manda armazenar as palavras que ja foram usadas na lista
+#--------VARIAVEIS-----------
+vidas = 5
+fim = 0
+#----------------------------
+
+
+while fim != 1:
+    while vidas !=0:
+        Funcoes.cls()
+        print("Dica: ",Funcoes.dica)
+        print(Funcoes._palavra_secreta)
+        Funcoes.exibirBoneco(vidas)
+        print(Funcoes._Palavra_secreta_pontilhada,"\n")
+        tentativa = input("Tentativa: ").upper()
+        retorno = Funcoes.palavraValida(tentativa)
+        if retorno == 1:
+            Funcoes.cls()
+            print(' VOCÊ VENCEU!')
+            break
+        elif retorno == 3:
+            continue
+        elif retorno == 2:
+            vidas -= 1
+
+    if(vidas == 0):
+        Funcoes.cls()
+        Funcoes.exibirBoneco(vidas)
+        print('\nVOCÊ PERDEU!!')
+    print('''
+    Deseja jogar novamente?
+    [1] SIM
+    [2] NÃO
+    ''')
+    reset = int(input())
+    if reset != 1:
+        fim = 1
+    else:
+        Funcoes.reset()
+        vidas = 5
+        fim = 0
--- Funcoes.py
+++ Funcoes.py
@@ -1,49 +1,92 @@
 import random
 import os
+import time
 from unicodedata import normalize
 
-Lista_Palavras_Usadas = [ ] #Armazena as palavras que já foram usadas
+global _palavra_secreta,_Palavra_secreta_pontilhada,dica
 
-def Start(): #seleciona uma palavra da lista
-    palavra = random.choices(Lista_Palavras)
-    numeroRandom = random.randrange(0, len(palavra))
-    palavra_Selecionada = palavra[numeroRandom].upper()
-    palavra_Selecionada = remover_acentos(palavra_Selecionada)
-    return palavra_Selecionada
+Lista_Palavras_Usadas = list()
+Lista_Letras_Usadas = list()
+acerta = []
+posicao = []
+aux = []
+
+
+def welcome():
+    print('''
+            BEM VINDO AO JOGO DA FORCA
+        
+        SELECIONE O TEMA:
+
+        [1] PROFISSÃO
+        [2] COMIDA
+        [3] CORPO HUMANO
+        [4] ANIMAIS
+        [5] INSTRUMENTOS
+        [6]CORES
+        [7]ALEATÓRIO
+
+        OPÇÃO: ''',end = '')
+    opcao = int(input())
+    return SelecionarTema(opcao)
+
 
 def remover_acentos(txt):
 
     return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')   
 
-def cls(): #Limpa tela
+def cls(): 
     os.system("cls")
 
-def palavra(string,palavrasecreta,contvidas):
-
-    if len(string) > 1:  # decide se a string informada é uma palavra ou uma letra
-        if string == palavrasecreta:  # se a palavra informada for igual a palavra secreta, o usuário ganha
-            venceu=True
+def verificacao(tentativa):
+    global _Palavra_secreta_pontilhada
+    
+    if len(tentativa) > 1:  
+        if tentativa == _palavra_secreta: 
+            venceu=1
             return venceu
-        elif palavra in Lista_Palavras_Usadas:  # caso a palavra informada seja diferente da secreta, ele percorre a lista de palavras usadas e as compara
+        elif tentativa in Lista_Palavras_Usadas:  
             print("Palavra ja utilizada!")
-        else:  # caso a palavra não esteja correta, nem tenha sido utilizada, uma vida é retirada do jogador, e a palavra é adicionada a lista de palavras usadas
+        else:  
             print("Essa não é a palavra")
-            exibirBoneco(contvidas + 1)  # retira uma vida do jogador
-            Lista_Palavras_Usadas.append(palavra)
-            contvidas += 1
+            Lista_Palavras_Usadas.append(tentativa)
+        time.sleep(2)
+        return 2
+    else:# se for letra
+        posicao.clear()
+        if tentativa in Lista_Letras_Usadas:
+            print('Essa letra já foi escolhida')
+        elif tentativa not in _palavra_secreta:
+            vidas -= 1
+            print('Não possui a letra')
+            Lista_Letras_Usadas.append(tentativa)
+        else:
+            Lista_Letras_Usadas.append(tentativa)
+            aux = list(_Palavra_secreta_pontilhada)
+            for i, letra in enumerate(_palavra_secreta):
+                if letra == tentativa:
+                    posicao.append(i)
+            for index in posicao:
+                aux[index*2] = tentativa
+            _Palavra_secreta_pontilhada = "".join(aux)
+            if '_' not in _Palavra_secreta_pontilhada:
+                return 1
+            return 3
+        time.sleep(2)
+        return 2
 
-def palavraValida(string):
-    #Verifica se a string informada possui um número.
-    temnumero = any(char.isdigit() for char in string)
-    if temnumero == True: # Se tiver números no meio da string informada ele retorna que o dado é inválido
+
+def palavraValida(tentativa):
+    
+    if tentativa.isalpha(): 
+        return verificacao(tentativa)
+    else: 
         print ("Dado informado não é válido!")
-    else: #Se o dado for válido ele agora verifica se é apenas uma letra ou uma palavra e prossegue
-        palavra(string,palavrasecreta,contvidas)
+        return 2
 
-def exibirBoneco(vidas): #
-    #estagio = [ 
-    #Forca Inicial
-    if vidas == 0: 
+def exibirBoneco(vidas): 
+    
+    if vidas == 5: 
         print(" _________________________")
         print("/_________________))______/")
         print("| |  / /          ||")
@@ -59,9 +102,8 @@ def exibirBoneco(vidas): #
         print("| |")
         print("| |")
         print("| |")
-
-    #Forca 1
-    elif vidas == 1:
+    
+    elif vidas == 4:
         print(" _________________________")
         print("/_________________))______/")
         print("| |  / /          ||")
@@ -77,9 +119,8 @@ def exibirBoneco(vidas): #
         print("| |")
         print("| |")
         print("| |")
-
-    #Forca 2
-    elif vidas == 2:
+    
+    elif vidas == 3:
         print(" _________________________")
         print("/_________________))______/")
         print("| |  / /          ||")
@@ -95,9 +136,8 @@ def exibirBoneco(vidas): #
         print("| |")
         print("| |")
         print("| |")
-
-    #Forca 3
-    elif vidas == 3:
+    
+    elif vidas == 2:
         print(" _________________________")
         print("/_________________))______/")
         print("| |  / /          ||")
@@ -113,9 +153,8 @@ def exibirBoneco(vidas): #
         print("| |")
         print("| |")
         print("| |")
-
-    #Forca 4
-    elif vidas == 4:
+    
+    elif vidas == 1:
         print(" _________________________")
         print("/_________________))______/")
         print("| |  / /          ||")
@@ -131,9 +170,8 @@ def exibirBoneco(vidas): #
         print("| |")
         print("| |")
         print("| |")
-
-    #Forca 5
-    elif vidas == 5:
+    
+    elif vidas == 0:
         print(" _________________________")
         print("/_________________))______/")
         print("| |  / /          ||")
@@ -149,261 +187,112 @@ def exibirBoneco(vidas): #
         print("| |             /     \    ")
         print("| |            ‘       ‘   ")
         print("| |")
-    #]
 
-Lista_Palavras = [
-    #PROFISSÃO
-    'AGRICULTOR',
-    'ARQUITETA',
-    'ASTRONAUTA',
-    'BOMBEIRO',
-    'CANTOR',
-    'CAFEICULTOR',
-    'CABELEREIRO',
-    'CUTELEIRO',
-    'CONTADOR',
-    'DELEGADO',
-    'DESIGNER',
-    'ESCRITOR',
-    'ENFERMEIRA',
-    'ELETRICISTA',
-    'FARMACEUTICO',
-    'FEIRANTE',
-    'FORJADOR',
-    'FOTOGRÁFO',
-    'HUMORISTA',
-    'HOMEOPATA',
-    'GARIMPEIRO',
-    'GENETICISTA',
-    'GERENTE',
-    'GEOGRÁFO',
-    'ILUSTRADOR',
-    'INTÉRPRETE',
-    'JORNALISTA',
-    'LUTHIER',
-    'LAVRADOR',
-    'MÉDICO',
-    'MOTORISTA',
-    'NUTRICIONISTA',
-    'OURIVES',
-    'OTORRINOLARINGOLOGISTA',
-    'PADEIRO',
-    'PEDREIRO',
-    'PROFESSOR',
-    'POLICIAL',
-    'QUIROPRAXISTA',
-    'QUÍMICO',
-    'RECEPCIONOSTA',
-    'REDATOR',
-    'RADIALISTA',
-    'SUSHIMAN',
-    'SUPERVISOR',
-    'TAXISTA',
-    'UROLOGISTA',
-    'VIGILANTE',
-    'VENDEDOR',
-     #COMIDA
-    'ABACATE',
-    'ABACAXI',
-    'ARROZ',
-    'BATATA',
-    'BISCOITO',
-    'BRIGADEIRO',
-    'CANJICA',
-    'CAJU',
-    'CHIPS',
-    'DONUTS',
-    'DAMASCO',
-    'ERVILHA',
-    'ESCONDIDINHO',
-    'FRANGO',
-    'FETTUCCINE',
-    'FEIJÃO',
-    'GRAVIOLA',
-    'GELEIA',
-    'GENGIBRE',
-    'HAMBURGUER',
-    'IOGURTE',
-    'INHAME',
-    'JUJUBA',
-    'KIWI',
-    'LAGOSTA',
-    'LINGUIÇA',
-    'MACARRONADA',
-    'MACAXEIRA',
-    'MORANGO',
-    'MOQUECA',
-    'MUSSARELA',
-    'OVOMALTINE',
-    'OMELETE',
-    'OREGANO',
-    'OSTRA',
-    'PUDIM',
-    'PIZZA',
-    'QUEIJO',
-    'QUEIJADINHA',
-    'QUIABO',
-    'ROCAMBOLE',
-    'SALSICHA',
-    'SUSHI',
-    'TAIOBA',
-    'TALHARIM',
-    'VATAPÁ',
-    'VINAGRETE',
-    #CORPO HUMANO
-    'ABDOMEN',
-    'AMÍGDALA',
-    'AXILA',
-    'ARTICULAÇÃO',
-    'ARTÉRIAS',
-    'BAÇO',
-    'BARRIGA',
-    'BOCA',
-    'BRAÇO',
-    'CABELO',
-    'CABEÇA',
-    'CANELA',
-    'COTVELO',
-    'COLUNA',
-    'CÓRNEA',
-    'CORAÇÃO',
-    'COXA',
-    'DEDO',
-    'DENTE',
-    'DIAFRAGMA',
-    'DORSAL',
-    'ESTOMAGO',
-    'ENDOCÁRDIO',
-    'ESTOMAGO',
-    'ESQUELETO',
-    'EPIDERME',
-    'FARINGE',
-    'FALANGES',
-    'GARGANTA',
-    'GENGIVAS',
-    'GLANDES',
-    'HIPODERME',
-    'ÍRIS',
-    'INTESTINO',
-    'JOELHO',
-    'LÁBIOS',
-    'LINGUA',
-    'LARINGE',
-    'MAMILOS',
-    'MÚSCULOS',
-    'NARIZ',
-    'NÁDEGAS',
-    #ANIMAIS
-    'ANTA',
-    'ABELHA',
-    'ARARA',
-    'ALCE',
-    'ASNO',
-    'BAIACU',
-    'BALEIA',
-    'BORBOLETA',
-    'CABRA',
-    'CACHORRO',
-    'CAPIVARA',
-    'DROMEDÁRIO',
-    'ELEFANTE',
-    'ESQUILO',
-    'FAISÃO',
-    'GAIVOTA',
-    'GALO',
-    'HIENA',
-    'HAMSTER',
-    'IGUANA',
-    'JACARÉ',
-    'JABURU',
-    'LAGOSTA',
-    'LEÃO',
-    'MACACO',
-    'MURIÇOCA',
-    'ORNITORRINCO',
-    'OVELHA',
-    'PAPAGAIO',
-    'PIRANHA',
-    'QUATI',
-    'RAPOSA',
-    'ROUXINOL',
-    'SUCURI',
-    'SARDINHA',
-    'TUBARÃO',
-    'TUCANO',
-    'TAMANDUÁ',
-    'URSO',
-    'URUBU',
-    'VACA',
-    'VESPA',
-    'VEADO',
-    'ZEBRA',
-    #INSTRUMENTOS MUSICAIS
-    'ALAÚDE',
-    'ARCODEÃO',
-    'BAIXO',
-    'BANDOLIM',
-    'BATERIA',
-    'BAIXO',
-    'CAIXA',
-    'CARRILHÃO',
-    'CASTANHOLHAS',
-    'CHOCALHO',
-    'CLARINETE',
-    'CLARIM',
-    'CÍTARA',
-    'CORNETE',
-    'FAGOTE',
-    'FLAUTA',
-    'FLAUTIM',
-    'GAITA',
-    'GUITARRA',
-    'HARPA',
-    'OBOÉ',
-    'ORGÃO',
-    'PANDEIRO',
-    'PIANO',
-    'PÍFARO',
-    'SANFONA',
-    'SINTETIZADOR',
-    'SAXOFONE',
-    'TECLADO',
-    'TRIANGULO',
-    'TUBA',
-    'UKULELE',
-    'VIOLÃO',
-    'VIOLINO',
-    'VIOLA',
-    'XILOFONE',
-    #CORES 
-    'AZUL',
-    'ANIL',
-    'AMETISTA',
-    'AMARELO',
-    'BRANCO',
-    'CINZA',
-    'CASTANHO',
-    'CIANO',
-    'DOURADO',
-    'ESMERALDA' ,
-    'FÚCSIA',
-    'FERRUGEM',
-    'JADE',
-    'JAMBO',
-    'LARANJA',
-    'LILÁS',
-    'MARROM',
-    'MAFIM',
-    'MAGNETA',
-    'OURO',
-    'PRETO',
-    'PRATA',
-    'QUANTUM',
-    'ROSA',
-    'ROXO',
-    'SÉPIA',
-    'SALMÃO',
-    'TURQUESA',
-    'VERDE',
-    'VERMELHO',
-    'VIOLETA']
+def SelecionarTema(tema):
+    if (tema == 1):
+
+        PROFISSÃO = [ 'AGRICULTOR', 'ARQUITETA', 'ASTRONAUTA', 'BOMBEIRO', 'CANTOR', 'CAFEICULTOR',
+                    'CABELEREIRO', 'CUTELEIRO', 'CONTADOR', 'DELEGADO', 'DESIGNER', 'ESCRITOR',
+                    'ENFERMEIRA', 'ELETRICISTA', 'FARMACEUTICO', 'FEIRANTE', 'FORJADOR',
+                    'FOTOGRÁFO', 'HUMORISTA', 'HOMEOPATA', 'GARIMPEIRO', 'GENETICISTA','GERENTE',
+                    'GEOGRÁFO', 'ILUSTRADOR','INTÉRPRETE', 'JORNALISTA', 'LUTHIER', 'LAVRADOR', 'MÉDICO',
+                    'MOTORISTA', 'NUTRICIONISTA',  'OURIVES', 'OTORRINOLARINGOLOGISTA', 'PADEIRO',
+                    'PEDREIRO', 'PROFESSOR', 'POLICIAL', 'QUIROPRAXISTA', 'QUÍMICO', 'RECEPCIONOSTA',
+                    'REDATOR', 'RADIALISTA', 'SUSHIMAN', 'SUPERVISOR','TAXISTA', 'UROLOGISTA', 'VIGILANTE', 'VENDEDOR']
+        return random.choices(PROFISSÃO), "PROFISSÃO"
+    elif (tema == 2):
+
+        COMIDA = [ 'ABACATE','ABACAXI', 'ARROZ', 'BATATA','BISCOITO',
+                'BRIGADEIRO', 'CANJICA','CAJU','CHIPS', 'DONUTS',
+                'DAMASCO','ERVILHA', 'ESCONDIDINHO','FRANGO','FETTUCCINE',
+                'FEIJÃO', 'GRAVIOLA', 'GELEIA','GENGIBRE', 'HAMBURGUER',
+                'IOGURTE', 'INHAME','JUJUBA', 'KIWI','LAGOSTA','LINGUIÇA',
+                'MACARRONADA','MACAXEIRA','MORANGO','MOQUECA', 'MUSSARELA',
+                'OVOMALTINE', 'OMELETE', 'OREGANO', 'OSTRA','PUDIM','PIZZA',
+                'QUEIJO', 'QUEIJADINHA', 'QUIABO','ROCAMBOLE', 'SALSICHA',
+                'SUSHI','TAIOBA', 'TALHARIM', 'VATAPÁ','VINAGRETE']
+        return random.choices(COMIDA)
+    elif (tema == 3):
+
+        CORPO = ['ABDOMEN','AMÍGDALA', 'AXILA','ARTICULAÇÃO','ARTÉRIAS', 'BAÇO',
+                'BARRIGA', 'BOCA', 'BRAÇO','CABELO','CABEÇA','CANELA','COTVELO',
+                'COLUNA','CÓRNEA', 'CORAÇÃO', 'COXA', 'DEDO', 'DENTE', 'DIAFRAGMA',
+                'DORSAL', 'ESTOMAGO', 'ENDOCÁRDIO','ESTOMAGO', 'ESQUELETO',
+                'EPIDERME', 'FARINGE','FALANGES', 'GARGANTA', 'GENGIVAS', 'GLANDES',
+                'HIPODERME', 'ÍRIS', 'INTESTINO', 'JOELHO', 'LÁBIOS', 'LINGUA',
+                'LARINGE', 'MAMILOS', 'MÚSCULOS', 'NARIZ','NÁDEGAS']
+        return random.choices(CORPO)
+
+    elif (tema == 4):
+
+        ANIMAIS = [ 'ANTA','ABELHA', 'ARARA','ALCE','ASNO','BAIACU','BALEIA',
+                    'BORBOLETA','CABRA','CACHORRO', 'CAPIVARA','DROMEDÁRIO',
+                    'ELEFANTE','ESQUILO','FAISÃO','GAIVOTA','GALO','HIENA',
+                    'HAMSTER','IGUANA','JACARÉ','JABURU','LAGOSTA','LEÃO',
+                    'MACACO','MURIÇOCA','ORNITORRINCO','OVELHA','PAPAGAIO',
+                    'PIRANHA', 'QUATI','RAPOSA','ROUXINOL','SUCURI','SARDINHA',
+                    'TUBARÃO','TUCANO', 'TAMANDUÁ','URSO','URUBU', 'VACA',
+                    'VESPA','VEADO','ZEBRA']
+        return random.choices(ANIMAIS)
+    elif (tema == 5):
+
+        INSTRUMENTOS = [ 'ALAÚDE','ARCODEÃO','BAIXO','BANDOLIM','BATERIA',
+                        'BAIXO', 'CAIXA','CARRILHÃO','CASTANHOLHAS','CHOCALHO',
+                        'CLARINETE', 'CLARIM','CÍTARA','CORNETE','FAGOTE',
+                        'FLAUTA', 'FLAUTIM','GAITA', 'GUITARRA', 'HARPA',
+                        'OBOÉ', 'ORGÃO', 'PANDEIRO','PIANO','PÍFARO','SANFONA',
+                        'SAXOFONE','TECLADO','TRIANGULO','TUBA', 'UKULELE',
+                        'VIOLÃO', 'VIOLINO','VIOLA','XILOFONE']
+        return random.choices(ISNTRUMENTOS)
+
+    elif (tema == 6):
+
+        CORES = [  'AZUL', 'ANIL', 'AMETISTA','AMARELO','BRANCO','CINZA',
+                'CASTANHO','CIANO','DOURADO','ESMERALDA' ,'FÚCSIA',
+                'FERRUGEM', 'JADE','JAMBO','LARANJA','LILÁS','MARROM',
+                'MAFIM','MAGNETA','OURO','PRETO','PRATA', 'QUANTUM',
+                'ROSA', 'ROXO','SÉPIA', 'SALMÃO','TURQUESA','VERDE','VERMELHO','VIOLETA']
+        return random.choices(CORES)
+
+    elif (tema == 7):
+
+        ALEATORIO = ['AGRICULTOR', 'ARQUITETA', 'ASTRONAUTA', 'BOMBEIRO', 'CANTOR', 'CAFEICULTOR','CABELEREIRO', 'CUTELEIRO', 'CONTADOR', 'DELEGADO', 'DESIGNER', 'ESCRITOR',
+                    'ENFERMEIRA', 'ELETRICISTA', 'FARMACEUTICO', 'FEIRANTE', 'FORJADOR','FOTOGRÁFO', 'HUMORISTA', 'HOMEOPATA', 'GARIMPEIRO', 'GENETICISTA','GERENTE',
+                    'GEOGRÁFO', 'ILUSTRADOR','INTÉRPRETE', 'JORNALISTA', 'LUTHIER', 'LAVRADOR', 'MÉDICO','MOTORISTA', 'NUTRICIONISTA',  'OURIVES', 'OTORRINOLARINGOLOGISTA', 'PADEIRO',
+                    'PEDREIRO', 'PROFESSOR', 'POLICIAL', 'QUIROPRAXISTA', 'QUÍMICO', 'RECEPCIONOSTA','REDATOR', 'RADIALISTA', 'SUSHIMAN', 'SUPERVISOR','TAXISTA', 'UROLOGISTA', 'VIGILANTE', 'VENDEDOR',
+                    'ABACATE','ABACAXI', 'ARROZ', 'BATATA','BISCOITO','BRIGADEIRO', 'CANJICA','CAJU','CHIPS', 'DONUTS','DAMASCO','ERVILHA', 'ESCONDIDINHO','FRANGO','FETTUCCINE',
+                    'FEIJÃO', 'GRAVIOLA', 'GELEIA','GENGIBRE', 'HAMBURGUER','IOGURTE', 'INHAME','JUJUBA', 'KIWI','LAGOSTA','LINGUIÇA',
+                    'MACARRONADA','MACAXEIRA','MORANGO','MOQUECA', 'MUSSARELA','OVOMALTINE', 'OMELETE', 'OREGANO', 'OSTRA','PUDIM','PIZZA',
+                    'QUEIJO', 'QUEIJADINHA', 'QUIABO','ROCAMBOLE', 'SALSICHA','SUSHI','TAIOBA', 'TALHARIM', 'VATAPÁ','VINAGRETE','ABDOMEN','AMÍGDALA', 'AXILA','ARTICULAÇÃO','ARTÉRIAS', 'BAÇO',
+                    'BARRIGA', 'BOCA', 'BRAÇO','CABELO','CABEÇA','CANELA','COTVELO','COLUNA','CÓRNEA', 'CORAÇÃO', 'COXA', 'DEDO', 'DENTE', 'DIAFRAGMA',
+                    'DORSAL', 'ESTOMAGO', 'ENDOCÁRDIO','ESTOMAGO', 'ESQUELETO','EPIDERME', 'FARINGE','FALANGES', 'GARGANTA', 'GENGIVAS', 'GLANDES',
+                    'HIPODERME', 'ÍRIS', 'INTESTINO', 'JOELHO', 'LÁBIOS', 'LINGUA','LARINGE', 'MAMILOS', 'MÚSCULOS', 'NARIZ','NÁDEGAS','ANTA','ABELHA', 'ARARA','ALCE','ASNO','BAIACU','BALEIA',
+                    'BORBOLETA','CABRA','CACHORRO', 'CAPIVARA','DROMEDÁRIO','ELEFANTE','ESQUILO','FAISÃO','GAIVOTA','GALO','HIENA','HAMSTER','IGUANA','JACARÉ','JABURU','LAGOSTA','LEÃO',
+                    'MACACO','MURIÇOCA','ORNITORRINCO','OVELHA','PAPAGAIO','PIRANHA', 'QUATI','RAPOSA','ROUXINOL','SUCURI','SARDINHA','TUBARÃO','TUCANO', 'TAMANDUÁ','URSO','URUBU', 'VACA',
+                    'VESPA','VEADO','ZEBRA','ALAÚDE','ARCODEÃO','BAIXO','BANDOLIM','BATERIA','BAIXO', 'CAIXA','CARRILHÃO','CASTANHOLHAS','CHOCALHO',
+                    'CLARINETE', 'CLARIM','CÍTARA','CORNETE','FAGOTE','FLAUTA', 'FLAUTIM','GAITA', 'GUITARRA', 'HARPA','OBOÉ', 'ORGÃO', 'PANDEIRO','PIANO','PÍFARO','SANFONA',
+                    'SAXOFONE','TECLADO','TRIANGULO','TUBA', 'UKULELE','VIOLÃO', 'VIOLINO','VIOLA','XILOFONE','AZUL', 'ANIL', 'AMETISTA','AMARELO','BRANCO','CINZA',
+                    'CASTANHO','CIANO','DOURADO','ESMERALDA' ,'FÚCSIA','FERRUGEM', 'JADE','JAMBO','LARANJA','LILÁS','MARROM',
+                    'MAFIM','MAGNETA','OURO','PRETO','PRATA', 'QUANTUM','ROSA', 'ROXO','SÉPIA', 'SALMÃO','TURQUESA','VERDE','VERMELHO','VIOLETA']
+        return random.choices(ALEATORIO)
+        
+def Start(): 
+    global _Palavra_secreta_pontilhada,_palavra_secreta, dica
+    cls()
+    palavra,dica = welcome()
+    numeroRandom = random.randrange(0, len(palavra))
+    palavra_Selecionada = palavra[numeroRandom].upper()
+    palavra_Selecionada = remover_acentos(palavra_Selecionada)
+    return palavra_Selecionada,dica
+
+_palavra_secreta,dica = Start() # armazena a palavra secreta
+_Palavra_secreta_pontilhada = "_ " * len(_palavra_secreta) # armazena a palavra secreta só que em vez de letras, em _
+#!OK
+
+def reset():
+    global _Palavra_secreta_pontilhada,_palavra_secreta
+    aux,acerta,Lista_Letras_Usadas,Lista_Palavras_Usadas,posicao.clear()
+    _Palavra_secreta_pontilhada = " "
+    _palavra_secreta = " "
+    _palavra_secreta,dica = Start() # armazena a palavra secreta
+    _Palavra_secreta_pontilhada = "_ " * len(_palavra_secreta) 
